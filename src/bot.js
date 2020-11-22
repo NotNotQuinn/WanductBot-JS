9454b00735fd1e2dc27ea67e38c8c87ab1f48511
NotNotQuinn <Quinn.GitHub@gmail.com>
2020-11-22 01:48:14 -0600

    improve readibility with comments, add eval & jsontest commands, change 8ball question api

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var Promise = require('promise');
 var curl = require('curlrequest');
 const querystring = require('querystring');
-
+var parseArgs = require('minimist')
 console.log("Thanks to @Nimmy0 on twitch for helping me out.");
 // He's a cool dude.
 
@@ -53,23 +53,41 @@ client.on("message", (channel, tags, message, self) => {
 
     if( args[0].charAt(0).toLowerCase() == prefix) {
         // commands go here.
+
+        // Evel command to send js to chat, only I can use it
+        if ( args[0].toLowerCase() == prefix + "eval" && tags.username.toLowerCase() == "quinndt") {
+            try {evel_result = eval(args.slice(1).join(" "))} catch (e) {
+                client.say(channel, `Error in eval: ${e}`)
+                return;
+            } 
+            client.say(channel, `Eval returned: ${evel_result}`)
+        };
+        // jsontest to see how args are parsed
+        if ( args[0].toLowerCase() == prefix + "jsontest" && tags.username.toLowerCase() == "quinndt") {
+            parsed_args = parseArgs(args.slice(1))
+            client.say(channel, JSON.stringify(parsed_args))
+        };
+        // Google command to link a google search with your query, because im lazy
         if ( args[0].toLowerCase() == prefix + "google") {
+            if (!args[1]) {client.say(channel, "Please provide something to google :)");return;};
             google_url = querystring.escape(args.join(" ").slice(prefix.length + "google ".length))
             google_url = `https://google.com/search?q=${google_url}`
             client.say(channel, `Here is your google search: ${google_url} :)`)
         };
+        // 8ball command can bring some fun to chats
         if ( args[0].toLowerCase() == prefix + "8ball") {
             if (!args[1]) {client.say(channel, "/me No question privided! ⚠ ⚠ ");return;};
-            client.say(channel, ":z loading...")
-            curl.request({url: "https://aidenwallis.co.uk/twitch/8ball.php"}, (err, responce) => {
+            ball8_question = querystring.escape(args.join(" ").slice(prefix.length + "8ball ".length))
+            curl.request({url: `https://8ball.delegator.com/magic/JSON/${ball8_question}`}, (err, responce) => {
                     if (err) {
-                        client.say(channel, `/me Failed to get answer ⚠ ⚠ error: ${err}`)
-                        console.log(err)
+                        client.say(channel, `/me Failed to get answer ⚠ ⚠ error: ${err}`);
+                        console.log(err);
                     } else {
-                        client.say(channel, `/me ${responce} :z ...`)
+                        client.say(channel, `/me ${JSON.parse(responce)['magic']['answer']} :z ...`);
                     };
             });
         };
+        // I always find thes interesting
         if ( args[0].toLowerCase() == prefix + "randomfact") {
             client.say(channel, "...")
             curl.request({url: "https://uselessfacts.jsph.pl/random.json?language=en"}, (err, data) => {
@@ -81,7 +99,9 @@ client.on("message", (channel, tags, message, self) => {
                 };
             });
         };
+        // I need to finish this command, but I am putting it off because I will have to update it as a develop
         if ( args[0].toLowerCase() == prefix + "help" || args[0].toLowerCase() == prefix + "commands") {client.say(channel, "help command coming soon BroBalt")}
+        // bot command to tell the user about the bot and to join and part channels
         if ( args[0].toLowerCase() == prefix + "bot" && tags.username.toLowerCase() == "quinndt" ) {
             switch (args[1]) {
                 case "join":
@@ -96,6 +116,7 @@ client.on("message", (channel, tags, message, self) => {
                         });
                     };
                     break;
+                // THIS DOESNT WORK 
                 case "part":
                     if (!/(#|)[a-z0-9_]/gi.test(args[2])) {
                         client.say(channel, "That doesn't seem like a username... TPFufun");
@@ -110,10 +131,16 @@ client.on("message", (channel, tags, message, self) => {
                     };
                     break;
                 default:
-                    client.say(channel, "/me Invalid subcommand. ⚠ ⚠ ")
+                    if (tags.username.toLowerCase() == "quinndt") {
+                        client.say(channel, "/me Invalid subcommand. ⚠ ⚠ ")
+                    } else {
+                        client.say(channel, "/me Invalid subcommand. ⚠ ⚠ ");
+                    };
+                    
             };
         };
-        if ( args[0].toLowerCase() == prefix + "set") {
+        // not used much, used to set the prefix 
+        if ( args[0].toLowerCase() == prefix + "set" && tags.username.toLowerCase() == "quinndt") {
             switch (args[1]) {
                 case "prefix":
                     if ( !/([\w.\/]|..)/gi.test(args[2]) )  {
@@ -134,8 +161,9 @@ client.on("message", (channel, tags, message, self) => {
             client.say(channel, args.join(" ").slice(prefix.length + 5)) // shit implemtation (maybe not?)
         };
     };
-    // These are special commands.
+    // "test" replies with "icles" its a little silly, but I like it
     if (args[0] === "test" && tags.username.toLowerCase() !== "turtoise" && tags.username.toLowerCase() !== "snappingbot") {client.say(channel, "icles");};
+    // theres basicaly 2 ping  commands because im lazy
     if ((args[0].toLowerCase() == ">ping" || args[0].toLowerCase() == prefix + "ping") && (tags.username.toLowerCase() !== "turtoise" && tags.username.toLowerCase() !== "snappingbot")) {
         duration_bot_uptime = moment.duration(moment().diff(bot_start_time));
         bot_uptime_text = duration_bot_uptime.seconds() + " seconds"
@@ -151,7 +179,8 @@ client.on("message", (channel, tags, message, self) => {
         };
         client.say(channel, `${pings} Pongs! B) PowerUpR Current prefix: [ ${prefix} ]. ${client.channels.length} channels joined. Bot online for ${bot_uptime_text}.`);
         fs.writeFile('pings.json', JSON.stringify([pings]), (err) => {if (err) throw err;});
-    } else if ((args[0].toLowerCase() == ">ping" || args[0].toLowerCase() == prefix + "ping") && (tags.username.toLowerCase() == "turtoise" || tags.username.toLowerCase() == "snappingbot")) {
+    } else /* this is because I dont want turt to spam the ping just to get the number up */ 
+    if ((args[0].toLowerCase() == ">ping" || args[0].toLowerCase() == prefix + "ping") && (tags.username.toLowerCase() == "turtoise" || tags.username.toLowerCase() == "snappingbot")) {
         duration_bot_uptime = moment.duration(moment().diff(bot_start_time));
         bot_uptime_text = duration_bot_uptime.seconds() + " seconds"
         if (duration_bot_uptime.minutes()) {
